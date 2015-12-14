37668e0a0e33050e2a8845dc7ec3ba7803d8e15c
Timotei Dolean <timotei21@gmail.com>
2015-12-14 13:40:14 +0200

    Properly handle non-Identifier Attribute names

--- NUnit3Migration.Tests/Processors/TestCaseAttributeProcessorTests.cs
+++ NUnit3Migration.Tests/Processors/TestCaseAttributeProcessorTests.cs
@@ -6,26 +6,24 @@ namespace NUnit3Migration.Tests.Processors
 {
     internal class TestCaseAttributeProcessorTests
     {
-        [TestCase(@"
-                [TestCase("""", false, Result= """")]
-                [TestCase(""2"", false, Result= ""2"")]
-                public string DoTest(string parameter, bool other)
-                {
-                    //Arrange
-                    //Act
-                    //Assert
-                    Assert.IsTrue(true);
-                }",
-             @"
-                [TestCase("""", false, ExpectedResult= """")]
-                [TestCase(""2"", false, ExpectedResult= ""2"")]
-                public string DoTest(string parameter, bool other)
-                {
-                    //Arrange
-                    //Act
-                    //Assert
-                    Assert.IsTrue(true);
-                }")]
+        [TestCase(
+            @"[TestCase("""", false, Result= """")]
+            [TestCase(""2"", false, Result = ""2"")]
+            public string DoTest(string parameter, bool other) {}",
+            @"[TestCase("""", false, ExpectedResult= """")]
+            [TestCase(""2"", false, ExpectedResult = ""2"")]
+            public string DoTest(string parameter, bool other) {}")]
+
+        [TestCase(
+            @"[System.Diagnostics.DebuggerNonUserCodeAttribute()]
+            [System.CodeDom.Compiler.GeneratedCodeAttribute(""PresentationBuildTasks"", ""4.0.0.0"")]
+            [System.ComponentModel.EditorBrowsableAttribute(System.ComponentModel.EditorBrowsableState.Never)]
+            public sealed class GeneratedInternalTypeHelper : System.Windows.Markup.InternalTypeHelper{}",
+            @"[System.Diagnostics.DebuggerNonUserCodeAttribute()]
+            [System.CodeDom.Compiler.GeneratedCodeAttribute(""PresentationBuildTasks"", ""4.0.0.0"")]
+            [System.ComponentModel.EditorBrowsableAttribute(System.ComponentModel.EditorBrowsableState.Never)]
+            public sealed class GeneratedInternalTypeHelper : System.Windows.Markup.InternalTypeHelper{}")]
+
         public async Task Process_ResultParameter_IsTransformedToExpectedResult(
             string initialCode, string expectedTransformedCode)
         {
