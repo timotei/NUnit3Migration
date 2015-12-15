5a681c94930ba9af88578ac3e8c47e842681f816
Timotei Dolean <timotei21@gmail.com>
2015-12-15 11:21:37 +0200

    Add the IgnoreAttributeProcessor (WIP)

---
+++ NUnit3Migration.Tests/Processors/IgnoreAttributeProcessorTests.cs
@@ -0,0 +1,27 @@
+﻿using NUnit.Framework;
+using NUnit3Migration.Processors;
+using System.Threading.Tasks;
+
+namespace NUnit3Migration.Tests.Processors
+{
+    internal class IgnoreAttributeProcessorTests
+    {
+        [TestCase(
+            @"[Ignore]
+            public string DoTest(string parameter, bool other) {}",
+            @"[Ignore(""No reason"")]
+            public string DoTest(string parameter, bool other) {}")]
+        [TestCase(
+            @"[Ignore()]
+            public string DoTest(string parameter, bool other) {}",
+            @"[Ignore(""No reason"")]
+            public string DoTest(string parameter, bool other) {}")]
+        public async Task Process_IgnoreAttributeWithoutReason_AddsPlaceholderText(
+            string initialCode, string expectedTransformedCode)
+        {
+            string transformedString = await Program.Process(new[] { new IgnoreAttributeProcessor() }, initialCode);
+
+            Assert.AreEqual(expectedTransformedCode, transformedString);
+        }
+    }
+}
---
+++ NUnit3Migration/Processors/IgnoreAttributeProcessor.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Editing;
+using System.Linq;
+
+namespace NUnit3Migration.Processors
+{
+    public class IgnoreAttributeProcessor : IProcessor
+    {
+        public void Process(DocumentEditor editor)
+        {
+            foreach (var node in editor.OriginalRoot.DescendantNodes()
+                .OfType<AttributeSyntax>()
+                .Where(attribute =>
+                    attribute.Name is IdentifierNameSyntax &&
+                    ((IdentifierNameSyntax)attribute.Name).Identifier.Text == "Ignore"))
+            {
+                if (node.ArgumentList == null ||
+                    node.ArgumentList.Arguments.Count == 0)
+                {
+                    //var argumentList = SyntaxFactory.AttributeArgumentList(
+                    //    SyntaxFactory.SingletonSeparatedList(
+                    //        SyntaxFactory.AttributeArgument(
+                    //            SyntaxFactory.LiteralExpression(
+                    //                SyntaxKind.StringLiteralExpression,
+                    //                SyntaxFactory.Literal(
+                    //                    SyntaxFactory.TriviaList(),
+                    //                    @"asd",
+                    //                    @"""asd""",
+                    //                    SyntaxFactory.TriviaList())))));
+
+                   // editor.ReplaceNode(node, node.WithName(SyntaxFactory.IdentifierName("asd")));
+                }
+            }
+        }
+    }
+}
