8f710caeb8cdfc6c9e6c84fcfdcbdbf055b038a0
SJD <selony@users.noreply.github.com>
2016-10-31 22:35:25 +0200

    Update AssertProcessor.cs

--- NUnit3Migration/Processors/AssertProcessor.cs
+++ NUnit3Migration/Processors/AssertProcessor.cs
@@ -17,46 +17,52 @@ namespace NUnit3Migration.Processors
                 if (expressionString == "Assert.IsNullOrEmpty" ||
                     expressionString == "Assert.IsNotNullOrEmpty")
                 {
-                    var expression = SyntaxFactory.MemberAccessExpression(
-                        SyntaxKind.SimpleMemberAccessExpression,
-                        SyntaxFactory.IdentifierName(
-                            SyntaxFactory.Identifier(
-                                node.Expression.GetLeadingTrivia(), "Assert", SyntaxTriviaList.Empty)),
-                        SyntaxFactory.IdentifierName("That"));
-
-                    var argumentList = SyntaxFactory.ArgumentList(
-                        SyntaxFactory.SeparatedList<ArgumentSyntax>(
-                            new SyntaxNodeOrToken[]
-                            {
-                                node.ArgumentList.Arguments.First(),
-                                SyntaxFactory.Token(
-                                    SyntaxFactory.TriviaList(),
-                                    SyntaxKind.CommaToken,
-                                    SyntaxFactory.TriviaList(SyntaxFactory.Space)),
-                                SyntaxFactory.Argument(
-                                    SyntaxFactory.MemberAccessExpression(
-                                        SyntaxKind.SimpleMemberAccessExpression,
-                                        SyntaxFactory.MemberAccessExpression(
-                                            SyntaxKind.SimpleMemberAccessExpression,
-                                            SyntaxFactory.MemberAccessExpression(
-                                                SyntaxKind.SimpleMemberAccessExpression,
-                                                GetAssertion(expressionString),
-                                                SyntaxFactory.IdentifierName("Null"))
-                                                .WithOperatorToken(SyntaxFactory.Token(SyntaxKind.DotToken)),
-                                            SyntaxFactory.IdentifierName("Or"))
-                                            .WithOperatorToken(SyntaxFactory.Token(SyntaxKind.DotToken)),
-                                        SyntaxFactory.IdentifierName("Empty"))
-                                        .WithOperatorToken(SyntaxFactory.Token(SyntaxKind.DotToken)))
-                            }));
-
-                    editor.ReplaceNode(node, node.WithExpression(expression).WithArgumentList(argumentList));
+                    HandleAssertNullOrEmpty(editor, expressionString);
                 }
             }
+        }
+        
+        private void HandleAssertNullOrEmpty(DocumentEditor editor, string expressionString)
+        {
+            var expression = SyntaxFactory.MemberAccessExpression(
+                    SyntaxKind.SimpleMemberAccessExpression,
+                    SyntaxFactory.IdentifierName(
+                        SyntaxFactory.Identifier(
+                            node.Expression.GetLeadingTrivia(), "Assert", SyntaxTriviaList.Empty)),
+                    SyntaxFactory.IdentifierName("That"));
+            var assertion = GetAssertion(expressionString);
+
+            var argumentList = SyntaxFactory.ArgumentList(
+                SyntaxFactory.SeparatedList<ArgumentSyntax>(
+                    new SyntaxNodeOrToken[]
+                    {
+                        node.ArgumentList.Arguments.First(),
+                        SyntaxFactory.Token(
+                            SyntaxFactory.TriviaList(),
+                            SyntaxKind.CommaToken,
+                            SyntaxFactory.TriviaList(SyntaxFactory.Space)),
+                        SyntaxFactory.Argument(
+                            SyntaxFactory.MemberAccessExpression(
+                                SyntaxKind.SimpleMemberAccessExpression,
+                                SyntaxFactory.MemberAccessExpression(
+                                    SyntaxKind.SimpleMemberAccessExpression,
+                                    SyntaxFactory.MemberAccessExpression(
+                                        SyntaxKind.SimpleMemberAccessExpression,
+                                        assertion,
+                                        SyntaxFactory.IdentifierName("Null"))
+                                        .WithOperatorToken(SyntaxFactory.Token(SyntaxKind.DotToken)),
+                                    SyntaxFactory.IdentifierName("Or"))
+                                    .WithOperatorToken(SyntaxFactory.Token(SyntaxKind.DotToken)),
+                                SyntaxFactory.IdentifierName("Empty"))
+                                .WithOperatorToken(SyntaxFactory.Token(SyntaxKind.DotToken)))
+                    }));
+
+            editor.ReplaceNode(node, node.WithExpression(expression).WithArgumentList(argumentList));
         }
 
         private ExpressionSyntax GetAssertion(string expressionString)
         {
-            if (!expressionString.EndsWith("IsNotNullOrEmpty"))
+            if (!expressionString.Contains("IsNot"))
                 return SyntaxFactory.IdentifierName("Is");
 
             return SyntaxFactory.MemberAccessExpression(
